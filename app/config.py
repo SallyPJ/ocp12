9f323e2bfcbf1eda92c2bbb42c3e72fce6936692
Sally PJ <polonio.sally@gmail.com>
2025-02-25 01:29:55 +0100

    chore: add Sentry for error tracking and Rich for enhanced CLI output

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
 from dotenv import load_dotenv
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
 # ✅ Load environment variables from .env file
 load_dotenv()
@@ -11,3 +14,14 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 ACCESS_TOKEN_EXPIRES_IN = 600 # 10 minutes
 REFRESH_TOKEN_EXPIRES_IN = 86400 # 24 hours
+
+# 📌 Intégration de Sentry avec SQLAlchemy et Logging
+sentry_sdk.init(
+    dsn="https://59fa82cbee5bf596c98f22c1a22b456c@o4508876971769856.ingest.de.sentry.io/4508876976029776",  # Remplace par ton DSN personnel Sentry
+    traces_sample_rate=1.0,  # Active la capture des performances
+    profiles_sample_rate=1.0,  # Active le profiling
+    integrations=[
+        LoggingIntegration(level="INFO", event_level="ERROR"),  # Capture des logs
+        SqlalchemyIntegration()  # Capture des requêtes SQL
+    ]
+)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from cli import cli
 
 if __name__ == "__main__":
+    division_by_zero = 1 / 0
+
     cli()
